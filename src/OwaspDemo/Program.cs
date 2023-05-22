c97abe45a33853127a72f4ecd74ddcea60831b8b
Unai Zorrilla Castro <uzorrilla@plainconcepts.com>
2023-05-22 16:38:05 +0200

    added security headers control

--- src/OwaspDemo/Program.cs
+++ src/OwaspDemo/Program.cs
@@ -5,6 +5,40 @@ builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
+//---------> SECURITY HEADERS
+
+var policyCollection = new HeaderPolicyCollection()
+        .AddFrameOptionsDeny()
+        .AddXssProtectionBlock()
+        .AddContentTypeOptionsNoSniff()
+        .AddStrictTransportSecurityMaxAgeIncludeSubDomains(maxAgeInSeconds: 60 * 60 * 24 * 365) // maxage = one year in seconds
+        .AddReferrerPolicyStrictOriginWhenCrossOrigin()
+        .RemoveServerHeader()
+        .AddContentSecurityPolicy(builder =>
+        {
+            builder.AddObjectSrc().None();
+            builder.AddFormAction().Self();
+            builder.AddFrameAncestors().None();
+        })
+        .AddCrossOriginOpenerPolicy(builder =>
+        {
+            builder.SameOrigin();
+        })
+        .AddCrossOriginEmbedderPolicy(builder =>
+        {
+            builder.RequireCorp();
+        })
+        .AddCrossOriginResourcePolicy(builder =>
+        {
+            builder.SameOrigin();
+        })
+        .AddCustomHeader("X-My-Test-Header", "Header value");
+
+app.UseSecurityHeaders(policyCollection);
+
+//---------> SECURITY HEADERS
+
+
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
